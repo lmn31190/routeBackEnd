ec2897d699ab624f68f41852ffd3d0ce3b1ac5b0
unknown <monielouis31@gmail.com>
2026-01-21 23:09:14 +0100

    CORS allow

--- server.py
+++ server.py
@@ -717,8 +717,9 @@ app.include_router(api_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_origins=os.environ.get('CORS_ORIGINS', 'http://localhost:3000').split(','),
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
